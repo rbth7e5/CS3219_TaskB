d629d9dad3ddf11572ee9d987fc413a97b7a799b
Caijie <caijie96@gmail.com>
2020-09-23 20:22:49 +0800

    Fix tests

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,7 @@ describe("Contacts", () => {
     describe("GET /", () => {
         it("should get all contacts", (done) => {
             chai.request(app)
-                .get('/api/contacts/')
+                .get('/contacts/')
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
@@ -20,7 +20,7 @@ describe("Contacts", () => {
         it("should get a single contact", (done) => {
             const id = "5f6ab423d135730b11bab8d4";
             chai.request(app)
-                .get(`/api/contacts/${id}`)
+                .get(`/contacts/${id}`)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.data.should.be.a('object');
@@ -31,7 +31,7 @@ describe("Contacts", () => {
         it("should not get any contact", (done) => {
             const id = "wrongid";
             chai.request(app)
-                .get(`/api/contacts/${id}`)
+                .get(`/contacts/${id}`)
                 .end((err, res) => {
                     res.should.have.status(404);
                     done();
